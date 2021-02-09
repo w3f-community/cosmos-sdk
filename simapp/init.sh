2ae2cfa2449ea1d956831b7e146c62a81fa74fbf
Mr-Leshiy <leshiy12345678@gmail.com>
2021-02-09 16:07:29 +0200

    update

--- simapp/init.sh
+++ simapp/init.sh
@@ -14,20 +14,15 @@ nsd init test --chain-id=namechain
 # nscli --chain-id namechain --output json keys add jack
 # nscli --chain-id namechain --output json keys add alice
 
-export KEYPASSWD='12345678'
+nscli keys add jack
 
-export ADDRESS='cosmos1s5vsakgzyjht4umu2zeqg3qlxceu7a0n98dl9a'
-export SEED='hood depend mandate catalog minute close glare indicate surge lawn young sauce various chat rain quit airport battle glance father apart loud idea wagon'
-(echo $SEED; echo $KEYPASSWD; echo $KEYPASSWD) | nscli --chain-id namechain --output json keys add jack --recover
+nscli keys add alice
 
-export ADDRESS='cosmos1na88jjrtm4kfg4y8lqcqqqejz5aclncd5qymv8'
-export SEED='riot evoke document section leg security smart velvet island where crouch dream ribbon lizard rabbit safe gas slow vivid correct term delay vital divorce'
-(echo $SEED; echo $KEYPASSWD; echo $KEYPASSWD) | nscli --chain-id namechain --output json keys add alice --recover
 
-echo $KEYPASSWD | nsd add-genesis-account $(nscli keys show jack -a) 1000nametoken,100000000stake
-echo $KEYPASSWD | nsd add-genesis-account $(nscli keys show alice -a) 1000nametoken,100000000stake
+nsd add-genesis-account $(nscli keys show jack -a) 1000nametoken,100000000stake
+nsd add-genesis-account $(nscli keys show alice -a) 1000nametoken,100000000stake
 
-echo $KEYPASSWD | nsd gentx jack --chain-id namechain
+nsd gentx jack --chain-id namechain
 
 echo "Collecting genesis txs..."
 nsd collect-gentxs
